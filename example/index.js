bcdea9441fb2112c60596e17d91398db4d8b9f8f
Ivar Johansson <ivar@webbmaffian.se>
2023-07-17 11:51:31 +0200

    Connect lazily

--- example/index.js
+++ example/index.js
@@ -1,35 +1,43 @@
-import { createTlsClient } from "../lib/client.js";
-import { createLogger } from "../lib/logger.js";
+import { createTlsClient, createLogger } from '../index.js';
 import { readFileSync } from 'fs';
 
 const client = createTlsClient({
-	// host: 'wm.log.center',
-	// port: 4610,
-	// key: readFileSync('./example/live/private.key'),
-	// cert: readFileSync('./example/live/certificate.pem'),
-	// ca: readFileSync('./example/live/root-ca.pem'),
-
-	host: '127.0.0.1',
+	host: 'wm.log.center',
 	port: 4610,
-	key: readFileSync('./example/local/private.key'),
-	cert: readFileSync('./example/local/certificate.pem'),
-	ca: readFileSync('./example/local/root-ca.pem'),
+	key: readFileSync('./example/live/private.key'),
+	cert: readFileSync('./example/live/certificate.pem'),
+	ca: readFileSync('./example/live/root-ca.pem'),
 });
+const logger = createLogger(client, 1687515986);
 
-const logger = createLogger(client, 1687784227);
+// const client = createTlsClient({
+// 	host: '127.0.0.1',
+// 	port: 4610,
+// 	key: readFileSync('./example/local/private.key'),
+// 	cert: readFileSync('./example/local/certificate.pem'),
+// 	ca: readFileSync('./example/local/root-ca.pem'),
+// });
+// const logger = createLogger(client, 1687784227);
 
-console.log('sent', logger.info('räksmörgås 1 %s', 'abc', 123, {
+console.log('sent', logger.info('node test 1 %s', 'abc', 123, {
 	foo: 'bar'
 }));
 
-console.log('sent', logger.info('räksmörgås 2 %s', 'abc', 123, {
+console.log('sent', logger.info('node test 2 %s', 'abc', 123, {
 	foo: 'bar'
 }));
 
-console.log('sent', logger.info('räksmörgås 3 %s', 'abc', 123, {
+console.log('sent', logger.info('node test 3 %s', 'abc', 123, {
 	foo: 'bar'
 }));
 
-// process.stdin.resume();
+// Server will disconnect after 60 seconds of inactivity - test if
+// the client reconnects when logging something after that.
+setTimeout(() => {
+	console.log('sent', logger.info('node test 4 %s', 'abc', 123, {
+		foo: 'bar'
+	}));
+}, 65 * 1000);
+
 
 
---
+++ index.js
@@ -0,0 +1,2 @@
+export { createTlsClient } from "./lib/client.js";
+export { createLogger } from "./lib/logger.js";
