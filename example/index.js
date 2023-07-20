72b649fa8b6b4a6506df35e5687e2cdd51cd5e68
Ivar Johansson <ivar@webbmaffian.se>
2023-07-20 13:34:23 +0200

    Clean-up

--- example/index.js
+++ example/index.js
@@ -2,22 +2,13 @@ import { createTlsClient, createLogger } from '../index.js';
 import { readFileSync } from 'fs';
 
 const client = createTlsClient({
-	host: 'wm.log.center',
+	host: '127.0.0.1',
 	port: 4610,
 	key: readFileSync('./example/live/private.key'),
 	cert: readFileSync('./example/live/certificate.pem'),
 	ca: readFileSync('./example/live/root-ca.pem'),
 });
 const logger = createLogger(client, 1687515986);
-
-// const client = createTlsClient({
-// 	host: '127.0.0.1',
-// 	port: 4610,
-// 	key: readFileSync('./example/local/private.key'),
-// 	cert: readFileSync('./example/local/certificate.pem'),
-// 	ca: readFileSync('./example/local/root-ca.pem'),
-// });
-// const logger = createLogger(client, 1687784227);
 
 console.log('sent', logger.info('node test 1 %s', 'abc', 123, {
 	foo: 'bar'
